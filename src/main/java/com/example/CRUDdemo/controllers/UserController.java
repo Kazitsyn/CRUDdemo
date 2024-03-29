1b38a486a09ea5cc402280f45387579498ae0d79
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2024-03-29 22:21:33 +0300

    integration

--- src/main/java/com/example/CRUDdemo/controllers/UserController.java
+++ src/main/java/com/example/CRUDdemo/controllers/UserController.java
@@ -1,33 +1,37 @@
 package com.example.CRUDdemo.controllers;
 import com.example.CRUDdemo.model.User;
+import com.example.CRUDdemo.service.FileGateway;
 import com.example.CRUDdemo.service.UserService;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
 @Log
+@RequiredArgsConstructor
 public class UserController {
 
     private final Counter requestCounter = Metrics.counter("add_count");
     private final UserService userService;
+    private final FileGateway fileGateway;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
     @GetMapping("/users")
     public String findAll(Model model){
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
+        fileGateway.writeToFile("GET\\users.txt", LocalDateTime.now().toString());
         return "user-list";
     }
 
@@ -41,12 +45,14 @@ public class UserController {
         userService.saveUser(user);
         log.info("user-create" + user);
         requestCounter.increment();
+        fileGateway.writeToFile("POST\\user-create.txt", user.toString() + LocalDateTime.now());
         return "redirect:/users";
     }
 
     @GetMapping("user-delete/{id}")
     public String deleteUser(@PathVariable("id") int id){
         userService.deleteById(id);
+        fileGateway.writeToFile("GET\\user-delete.txt", LocalDateTime.now().toString());
         log.info("user-delete");
         return "redirect:/users";
     }
@@ -59,6 +65,7 @@ public class UserController {
     @PostMapping("/user-update")
     public String updateUser(User user){
         userService.updateById(user);
+        fileGateway.writeToFile("POST\\user-update.txt", user.toString() + LocalDateTime.now());
         log.info("user-update" + user);
         return "redirect:/users";
     }
